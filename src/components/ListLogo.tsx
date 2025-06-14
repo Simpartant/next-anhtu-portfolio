4fe5fad780f1e1ff8b8ea4c57ab4007d78991bf4
Son Ta <sonta@Sons-Mac-mini.local>
2025-06-14 22:18:01 +0700

    fix list logo size

--- src/components/ListLogo.tsx
+++ src/components/ListLogo.tsx
@@ -21,7 +21,7 @@ export default function ListLogo() {
           key={logo.alt}
           src={logo.src}
           alt={logo.alt}
-          className={`h-auto object-contain w-30 lg:w-40${
+          className={`h-auto object-contain w-25 lg:w-40${
             idx >= 3 ? " max-sm:mt-3" : ""
           }`}
         />
