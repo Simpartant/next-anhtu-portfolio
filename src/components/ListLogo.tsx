d4562709a9e5a5f27bc8ce8fdf03494cff931a09
Son Ta <sonta@Sons-Mac-mini.local>
2025-06-15 10:05:08 +0700

    fix list logo size

--- src/components/ListLogo.tsx
+++ src/components/ListLogo.tsx
@@ -5,27 +5,34 @@ import Group3 from "@/assets/Group/Group 3.svg";
 import Group4 from "@/assets/Group/Group 4.svg";
 import Group5 from "@/assets/Group/Group 5.svg";
 
-const logos = [
-  { src: Group1, alt: "capital" },
-  { src: Group2, alt: "gamuda-land" },
-  { src: Group3, alt: "sun-group" },
-  { src: Group4, alt: "masterise" },
-  { src: Group5, alt: "angia" },
-];
-
 export default function ListLogo() {
   return (
     <div className="flex flex-wrap md:flex-nowrap justify-between items-center py-4 max-sm:px-6 md:py-20">
-      {logos.map((logo, idx) => (
-        <Image
-          key={logo.alt}
-          src={logo.src}
-          alt={logo.alt}
-          className={`h-auto object-contain w-45 max-sm:w-25${
-            idx >= 3 ? " max-sm:mt-3" : ""
-          }`}
-        />
-      ))}
+      <Image
+        src={Group1}
+        alt="capital"
+        className="h-auto object-contain w-24 lg:w-40"
+      />
+      <Image
+        src={Group2}
+        alt="gamuda-land"
+        className="h-auto object-contain w-24 lg:w-40"
+      />
+      <Image
+        src={Group3}
+        alt="sun-group"
+        className="h-auto object-contain w-24 lg:w-40"
+      />
+      <Image
+        src={Group4}
+        alt="masterise"
+        className="h-auto object-contain w-24 lg:w-40 max-sm:mt-3"
+      />
+      <Image
+        src={Group5}
+        alt="angia"
+        className="h-auto object-contain w-24 lg:w-40 max-sm:mt-3"
+      />
     </div>
   );
 }
