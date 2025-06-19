39c9fffdd3b97daf5e899bf698768907ce3f3c5b
Son Ta <sonta@Sons-Mac-mini.local>
2025-06-19 22:05:30 +0700

    refactor code in homepage

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,11 +1,6 @@
 import Blogs from "@/components/Blogs/Blogs";
-import Discover from "@/components/Discover";
 import Hero from "@/components/Hero";
 import ListLogo from "@/components/ListLogo";
-import ActionComponent from "@/components/ActionComponent";
-import ClientFeedback from "@/components/ClientFeedback";
-import ContactMe from "@/components/ContactMe";
-import Products from "@/components/Products/Products";
 
 export default function HomePage() {
   return (
