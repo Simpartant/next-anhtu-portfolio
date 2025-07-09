b131b06ac77363d27ab949a62708f4c670cc76a4
Son Ta <sonta@192.168.1.3.non-exists.ptr.local>
2025-07-09 11:53:37 +0700

    fix api get contact

--- src/app/api/contacts/[id]/route.ts
+++ src/app/api/contacts/[id]/route.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Contact from "@/models/Contact";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function GET(req: NextRequest) {
   await connectDB();
-  const { id } = context.params;
+
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // lấy id từ URL
 
   try {
     const contact = await Contact.findById(id).lean();
@@ -15,7 +14,6 @@ export async function GET(
       return NextResponse.json({ error: "Contact not found" }, { status: 404 });
     }
     return NextResponse.json({ data: contact });
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
